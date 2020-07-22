4df004ebe69477cadf0f54773e97454e406fb3d6
David Dennis <david.dc.dennis@gmail.com>
2020-07-21 21:24:28 -0500

    added dom.js and notes

--- loops.js
+++ loops.js
@@ -1,17 +1,53 @@
-"use strict";
+// "use strict";
 
-const colors = ['red', 'orange', 'blue', 'yellow', 'green'];
+// const colors = ['red', 'orange', 'blue', 'yellow', 'green'];
 
-const colorLoop = () => {
-    let domString = '';
+// const colorLoop = () => {
+//     let domString = '';
 
-    for (let i = 0; i < colors.length; i++) {
-        domString += `<h1>${colors[i]}</h1>`
+//     for (let i = 0; i < colors.length; i++) {
+//         domString += `<h1>${colors[i]}</h1>`
 
-        console.log(domString);
-    }
+//         console.log(domString);
+//     }
 
-    
-}
 
-colorLoop();
+// }
+
+// colorLoop(); 
+
+// console.log(quoteFinder('is')); //gives 7 because that is where 'is' starts in the given string.
+
+// const employee = {
+//     firstName: 'Bailey',
+//     lastName: 'Dennis',
+//     role: 'student',
+//     accountNumber: '2-458554842136',
+//     isManager: false,
+//     titles: ['student developer', 'coffee girl'],
+//     accolade: {
+//         title: 'EMS NICU Specialist',
+//         dateEarned: '6/15/2015',
+//         expires: 'Never',
+//         isCool: true
+//     }
+// }
+
+
+// console.log(employee.firstName);
+// const keyToCheck = 'accolade'
+
+// console.log(employee[keyToCheck]['isCool']); //variable usage requires bracket notation!
+
+// let caste = (obj) => {
+//     if (obj.firstName[0] === "B") {
+//         obj.status = 'VIP';
+//         console.log('VIP BABY');
+//     } else {
+//         obj.status = 'PEASANT';
+//         console.log('GET OUT PEASANT');
+//     }
+//     console.log(employee.status);
+// }
+
+// console.log(caste(employee));

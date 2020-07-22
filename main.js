1bd583c1ab1f8e0ec4c7796e25a89781529f04ab
David Dennis <david.dc.dennis@gmail.com>
2020-07-21 20:02:04 -0500

    added loops notes

--- main.js
+++ main.js
@@ -19,35 +19,36 @@
 
 // console.log(quoteFinder('is')); //gives 7 because that is where 'is' starts in the given string.
 
-const employee = {
-    firstName: 'Bailey',
-    lastName: 'Dennis',
-    role: 'student',
-    accountNumber: '2-458554842136',
-    isManager: false,
-    titles: ['student developer', 'coffee girl'],
-    accolade: {
-        title: 'EMS NICU Specialist',
-        dateEarned: '6/15/2015',
-        expires: 'Never',
-        isCool: true
-    }
-}
+// const employee = {
+//     firstName: 'Bailey',
+//     lastName: 'Dennis',
+//     role: 'student',
+//     accountNumber: '2-458554842136',
+//     isManager: false,
+//     titles: ['student developer', 'coffee girl'],
+//     accolade: {
+//         title: 'EMS NICU Specialist',
+//         dateEarned: '6/15/2015',
+//         expires: 'Never',
+//         isCool: true
+//     }
+// }
 
 // console.log(employee.firstName);
 // const keyToCheck = 'accolade'
 
 // console.log(employee[keyToCheck]['isCool']); //variable usage requires bracket notation!
 
-let caste = (obj) => {
-    if (obj.firstName[0] === "B") {
-        obj.status = 'VIP';
-        console.log('VIP BABY');
-    } else {
-        obj.status = 'PEASANT';
-        console.log('GET OUT PEASANT');
-    }
-    console.log(employee.status);
-}
+// let caste = (obj) => {
+//     if (obj.firstName[0] === "B") {
+//         obj.status = 'VIP';
+//         console.log('VIP BABY');
+//     } else {
+//         obj.status = 'PEASANT';
+//         console.log('GET OUT PEASANT');
+//     }
+//     console.log(employee.status);
+// }
 
-console.log(caste(employee));
+// console.log(caste(employee));
+
